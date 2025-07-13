dad70d7b2218da362bbd2f1dc08497cf19fa9b72
Kamil Trusiak <k.trusiak@outlook.com>
2025-07-13 23:41:34 +0200

    feat: add user profile page

--- src/pages/user-profile/UserProfilePage.tsx
+++ src/pages/user-profile/UserProfilePage.tsx
@@ -1,42 +1,85 @@
-import { useState } from "react";
 import { useTranslation } from "react-i18next";
+import { useNavigate } from "react-router";
 
-import viteLogo from "/vite.svg";
+import { Calendar, Email, Phone } from "@carbon/icons-react";
+import { Button, Heading, Section, Stack } from "@carbon/react";
+import { format } from "date-fns";
 
-import reactLogo from "../../assets/react.svg";
 import { useUserProfile } from "../../hooks/useUserProfile.ts";
+import { routes } from "../../routes.ts";
 
-import "../../App.css";
+import styles from "./UserProfilePage.module.scss";
 
 export function UserProfilePage() {
-  const [count, setCount] = useState(0);
-
+  const navigate = useNavigate();
   const { t } = useTranslation();
 
   const { data: profileData } = useUserProfile();
 
+  if (!profileData) {
+    return (
+      <Section>
+        <Stack gap={6}>
+          <Heading>{t("userProfile.welcome.heading")}</Heading>
+          <p>{t("userProfile.welcome.description")}</p>
+          <Button
+            type="button"
+            kind="primary"
+            onClick={() => navigate(routes.form)}
+          >
+            {t("userProfile.welcome.updateProfile")}
+          </Button>
+        </Stack>
+      </Section>
+    );
+  }
+
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          {t("counter", { count })}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Section as="article">
+      <Stack gap={6}>
+        <header className={styles.header}>
+          <div className={styles.avatarContainer}>
+            {profileData.avatarUrl && (
+              <img
+                alt={t("userProfile.avatar.alt")}
+                src={profileData.avatarUrl}
+                className={styles.avatar}
+              />
+            )}
+            {!profileData.avatarUrl && <div className={styles.placeholder} />}
+          </div>
+          <Heading>
+            {profileData.firstName} {profileData.lastName}
+          </Heading>
+        </header>
+
+        <section className={styles.container}>
+          <Section className={styles.aboutSection}>
+            <Stack gap={6}>
+              <Heading>{t("userProfile.about.heading")}</Heading>
+              <Section>{profileData.about}</Section>
+            </Stack>
+          </Section>
+
+          <Section className={styles.informationSection}>
+            <Stack gap={6}>
+              <Heading>{t("userProfile.info.heading")}</Heading>
+              <div className={styles.infoItem}>
+                <Calendar aria-label={t("userProfile.info.birthDate")} />
+                {format(profileData.birthDate, "MM/dd/yyyy")}
+              </div>
+              <div className={styles.infoItem}>
+                <Phone aria-label={t("userProfile.info.phoneNumber")} />
+                {profileData.phoneNumber}
+              </div>
+              <div className={styles.infoItem}>
+                <Email aria-label={t("userProfile.info.email")} />
+                {profileData.email}
+              </div>
+            </Stack>
+          </Section>
+        </section>
+      </Stack>
+    </Section>
   );
 }
