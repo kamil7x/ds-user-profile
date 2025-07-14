cd85ee28bb9db64fc81f64f49c6bcebb6914e2a4
Kamil Trusiak <k.trusiak@outlook.com>
2025-07-14 21:44:51 +0200

    fix: build issues

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,5 @@
+import { ConfigContextType } from "./context/config/ConfigContext.ts";
+
 export const config: ConfigContextType = {
   cookiePromptEnabled: import.meta.env.VITE_COOKIE_PROMPT_ENABLED === "true",
 };
